79827123d947c0f75b72ec101e76e05a5835d9ab
Akos Oravecz <oraveczakos2001@gmail.com>
2022-11-07 20:31:39 +0100

    create serializers for api

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category_list', views.get_categories, name='category_list'),
-    path('sample_list', views.get_samples, name='sample_list'),
+    path('category', views.get_categories, name='category_list'),
+    path('sample', views.get_samples, name='sample_list'),
+    path('rates', views.get_rates, name='rate_list'),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from . import models
-import json
+from django.http import JsonResponse
+from .models import Category, Sample, Rate
+from .serializers import CategorySerializer, SampleSerializer, RateSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(["GET", "POST"])
 def get_categories(request):
-    category_filter = {k: v for k, v in request.GET.items()}
-    res = json.dumps([str(c) for c in models.Category.objects.filter(**category_filter)])
-    return HttpResponse(res, content_type="application/json")
+    if request.method == "GET":
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return JsonResponse({"categories": serializer.data}, safe=False)
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@api_view(["GET", "POST"])
 def get_samples(request):
-    sample_filter = {k: v for k, v in request.GET.items()}
-    res = json.dumps([str(c) for c in models.Sample.objects.filter(**sample_filter)])
-    return HttpResponse(res, content_type="application/json")
+    if request.method == "GET":
+        samples = Sample.objects.all()
+        serializer = SampleSerializer(samples, many=True)
+        return JsonResponse({"samples": serializer.data}, safe=False)
+    elif request.method == "POST":
+        serializer = SampleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(["GET", "POST"])
+def get_rates(request):
+    if request.method == "GET":
+        rates = Rate.objects.all()
+        serializer = RateSerializer(rates, many=True)
+        return JsonResponse({"rates": serializer.data}, safe=False)
+    elif request.method == "POST":
+        serializer = RateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
