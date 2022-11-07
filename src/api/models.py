235b185bde439c1660db09ca7b90086cca9c9e23
Akos Oravecz <oraveczakos2001@gmail.com>
2022-11-07 19:44:36 +0100

    update models in api

--- src/api/models.py
+++ src/api/models.py
@@ -3,35 +3,39 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    description = models.CharField(max_length=1000, null=True)
-    img = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    img = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
 class Sample(models.Model):
     categoryId = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, null=True)
-    img = models.CharField(max_length=500, null=True)
-    tags = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    img = models.CharField(max_length=500, null=True, blank=True)
+    tags = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
 class Rate(models.Model):
-    class RateValue(models.IntegerChoices):
-        NOT_FILLED = -1
-        ONE = 1
-        TWO = 2
-        THREE = 3
-        FOUR = 4
-        FIVE = 5
+    rateValues = (
+        ("-1", -1),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5)
+    )
     
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     sampleId = models.ForeignKey(Sample, on_delete=models.CASCADE)
-    rate = models.SmallIntegerField(default=RateValue.NOT_FILLED, choices=RateValue.choices)
+    rate = models.SmallIntegerField(default=rateValues[0], choices=rateValues)
     datetime = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ("userId", "sampleId")
 
     def __str__(self):
         return f"user={self.userId},sample={self.sampleId},rate={self.rate}"
