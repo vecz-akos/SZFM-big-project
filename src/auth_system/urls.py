4fbbeabda2cec1c94e193eceb125964e862e96f6
Kerekes Konrád Péter <konraad.kerekes@gmail.com>
2022-11-07 19:02:49 +0100

    added welcome page to main page

--- src/auth_system/urls.py
+++ src/auth_system/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import HomePage, Welcome, Register, Login, logoutUser
 
 urlpatterns = [
+    path('', Welcome, name="welcome-page"),
     path('home/', HomePage, name="home-page"),
-    path('welcome/', Welcome, name="welcome-page"),
     path('register/', Register, name="register-page"),
     path('login/', Login, name="login-page"),
     path('logout/', logoutUser, name="logout"),
