753f1319a7cd7e9f270672d8b021b40f0a2eeb38
Kerekes Konrád Péter <konraad.kerekes@gmail.com>
2022-11-07 20:50:30 +0100

    delete old register and login page

--- src/auth_system/urls.py
+++ src/auth_system/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import HomePage, Welcome, Register, Login, logoutUser
+from .views import HomePage, Welcome, logoutUser
 
 urlpatterns = [
     path('', Welcome, name="welcome-page"),
     path('home/', HomePage, name="home-page"),
-    path('register/', Register, name="register-page"),
-    path('login/', Login, name="login-page"),
     path('logout/', logoutUser, name="logout"),
 ]
--- src/auth_system/views.py
+++ src/auth_system/views.py
@@ -40,38 +40,6 @@ def Welcome(request):
 
     return render(request, 'auth_system/welcome.html', {})
 
-def Register(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('fname')
-        last_name = request.POST.get('lname')
-        user_name = request.POST.get('uname')
-        email = request.POST.get('email')
-        password = request.POST.get('passw')
-
-        new_user = User.objects.create_user(user_name, email, password)
-        new_user.first_name = first_name
-        new_user.last_name = last_name
-
-        new_user.save()
-
-        return redirect('login-page')
-
-    return render(request, 'auth_system/register.html', {})
-
-def Login(request):
-    if request.method == 'POST':
-        user_name = request.POST.get('uname')
-        password = request.POST.get('passw')
-
-        user = authenticate(request, username=user_name, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home-page')
-        else:
-            return HttpResponse('Error, user does not exist')
-
-    return render(request, 'auth_system/login.html', {})
-
 def logoutUser(request):
     logout(request)
     return redirect('welcome-page')
