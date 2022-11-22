acbc3f030b439c2ea368b39465389f1c303cb406
Kerekes Konrád Péter <konraad.kerekes@gmail.com>
2022-11-22 11:01:40 +0100

    reco-sys - samples basic model

--- src/reco_system/models.py
+++ src/reco_system/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Samples(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField(max_length=300)
+    img = models.ImageField(
+        upload_to='imgs/', 
+        height_field='height', 
+        width_field='width'
+    ),
+    tags = models.CharField(max_length=50)
+    def __str__(self):
+        return str(self.pk)
