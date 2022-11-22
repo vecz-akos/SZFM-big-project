8d10f76d509075ae51bd40eeb9fd590665ea4fe8
Kerekes Konrád Péter <konraad.kerekes@gmail.com>
2022-11-22 11:09:56 +0100

    reco_sys - Rating basic model

--- src/reco_system/models.py
+++ src/reco_system/models.py
@@ -12,3 +12,9 @@ class Samples(models.Model):
     tags = models.CharField(max_length=50)
     def __str__(self):
         return str(self.pk)
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    sample = models.ForeignKey(Samples, on_delete=models.CASCADE, default=None)
+    rating = '' # HIANYOS
+    rated_date = models.DateTimeField(auto_now_add=True)
